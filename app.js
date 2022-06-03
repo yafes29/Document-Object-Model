f1e77b0cceb0c987ac4223ee8ed31a2fcf557fbf
mustafa <yafess229@gmail.com>
2022-06-03 20:34:57 +0300

    update

--- app.js
+++ app.js
@@ -10,3 +10,12 @@ console.log(window.location.hostname);
 console.log(window.innerWidth);
 
 console.log(window.outerWidth);
+
+let windows = document.all[3];
+
+console.log(windows);
+
+var wndw = document.scripts;
+
+
+console.log(wndw);

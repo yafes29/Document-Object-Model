6e94d55fc7ea9db21fde0d2923abc0b3d23cc3ec
mustafa <yafess229@gmail.com>
2022-06-14 17:43:06 +0300

    update

--- tek_çift_sayı/app.js
+++ tek_çift_sayı/app.js
@@ -1,6 +1,13 @@
-for(let a= 0; a <=100; a++){
-    if(a%2 ==0){
-        continue;
-    }
-    console.log(a);
-}
+// for(let a= 0; a <=100; a++){
+//     if(a%2 ==0){
+//         continue;
+//     }
+//     console.log(a);
+// }
+
+ for(let a= 0; a <=100; a++){
+     if(a == 50){
+    continue;
+     }
+     console.log(a);
+ }
